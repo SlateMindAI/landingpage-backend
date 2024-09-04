6c63b4cf03678533ac61522824af6131dbf01b2f
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2024-09-03 23:31:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Allows only this origin
+    origin: 'https://www.slatemindai.com/', // Allows only this origin
 }));
 
 app.use('/api', waitingListRoutes);
